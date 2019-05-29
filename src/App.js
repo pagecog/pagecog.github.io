68a66226476e029084a8a927b98216db8c2cd897
Nik Cubrilovic <nc9@protonmail.com>
2019-05-29 20:09:39 +0000

    setup basic website

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.png'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Coming Soon</h1>
+        <p><a href="mailto:contact@pagecog.com">contact@pagecog.com</a></p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
